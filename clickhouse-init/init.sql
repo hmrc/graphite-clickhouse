7402c09bf921a5194189acff940b2112c8132345
Robert White <robert.white@digital.hmrc.gov.uk>
2018-10-22 15:00:00 +0100

    TEL-811
    - Bring number of Clickhouse servers in cluster to three
    - Move metrics into table in metrics. database
    - Create Distributed tables in default. database
    - Add IF NOT EXISTS to init.sql CREATE TABLE commands
    - Extend feed-metrics to allow for pushing into multiple servers

--- clickhouse-init/init.sql
+++ clickhouse-init/init.sql
@@ -1,16 +1,25 @@
-CREATE TABLE graphite (
+CREATE DATABASE IF NOT EXISTS metrics;
+
+CREATE TABLE IF NOT EXISTS metrics.graphite (
   Path String,
   Value Float64,
   Time UInt32,
   Date Date,
   Timestamp UInt32
-) ENGINE = ReplicatedGraphiteMergeTree('/clickhouse/tables/{layer}-{shard}/hits', '{replica}', Date, (Path, Time), 8192, 'graphite_rollup');
+) ENGINE = GraphiteMergeTree(Date, (Path, Time), 8192, 'graphite_rollup');
 
 -- optional table for faster metric search
-CREATE TABLE graphite_tree (
+CREATE TABLE IF NOT EXISTS metrics.graphite_tree (
   Date Date,
   Level UInt32,
   Path String,
   Deleted UInt8,
   Version UInt32
-) ENGINE = ReplicatedReplacingMergeTree('/clickhouse/tables/{layer}-{shard}/hits', '{replica}', Date, (Level, Path), 8192, Version);
+) ENGINE = ReplacingMergeTree(Date, (Level, Path), 8192, Version);
+
+CREATE TABLE IF NOT EXISTS default.graphite AS metrics.graphite
+ENGINE = Distributed('hmrc_data_cluster', 'metrics', 'graphite');
+
+CREATE TABLE IF NOT EXISTS default.graphite_tree AS metrics.graphite_tree
+ENGINE = Distributed('hmrc_data_cluster', 'metrics', 'graphite_tree');
+
--- feed-metrics.sh
+++ feed-metrics.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 
-set -euxo pipefail
+set -euo pipefail
+
+INTERVAL=30
+PORTS="2003"
+
+# If this is Linux the netcat cr/lf option is different than on a Mac
+if [[ "$(uname -o)" =~ "GNU/Linux" ]]; then
+  NC_KEY=C
+fi
 
 while true
 do
-  echo "local.random.diceroll $((RANDOM % (6) + 1 )) `date +%s`" | nc -c localhost 2003
-  sleep 1
+  RAND_NUM=$((RANDOM % (6) + 1 ))
+  echo "local.random.diceroll ${RAND_NUM} `date +%s`"
+  for PORT in ${PORTS}; do
+    echo "local.random.diceroll ${RAND_NUM} `date +%s`" | nc -"${NC_KEY:-c}" localhost "${PORT}"
+  done
+  sleep ${INTERVAL}
 done
