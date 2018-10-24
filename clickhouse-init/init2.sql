59f1fb9816cc6fb0e49fdc359e4843356b99d9ef
Robert White <robert.white@digital.hmrc.gov.uk>
2018-10-24 11:38:45 +0100

    Update

--- clickhouse-init/init2.sql
+++ clickhouse-init/init2.sql
@@ -4,7 +4,9 @@ CREATE TABLE IF NOT EXISTS default.graphite ON CLUSTER hmrc_data_cluster (
   Time UInt32,
   Date Date,
   Timestamp UInt32
-) ENGINE = GraphiteMergeTree(Date, (Path, Time), 8192, 'graphite_rollup');
+) ENGINE = ReplicatedGraphiteMergeTree('/clickhouse/tables/{shard1}/graphite', '{replica1}', 'graphite_rollup')
+PARTITION BY toMonday(Date)
+ORDER BY (Path, Time);
 
 CREATE TABLE IF NOT EXISTS default.graphite_tree ON CLUSTER hmrc_data_cluster (
   Date Date,
@@ -12,7 +14,8 @@ CREATE TABLE IF NOT EXISTS default.graphite_tree ON CLUSTER hmrc_data_cluster (
   Path String,
   Deleted UInt8,
   Version UInt32
-) ENGINE = ReplacingMergeTree(Date, (Level, Path), 8192, Version);
+) ENGINE = ReplicatedReplacingMergeTree('/clickhouse/tables/{shard1}/graphite_tree', '{replica1}', 'graphite_rollup')
+ORDER BY (Level, Path);
 
 CREATE TABLE IF NOT EXISTS graphite_distributed AS default.graphite
 ENGINE = Distributed('hmrc_data_cluster', 'default', 'graphite');
